c9f5226cc4585c164e54954f2e1a2e38e7e6cd3e
wooder27 <jim20080903@gmail.com>
2024-11-07 11:47:44 +0800

    update add con

---
+++ Runtime/SceneControl.cs
@@ -0,0 +1,30 @@
+﻿using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace wooder.Tools
+{
+    /// <summary>
+    /// 場景控制器 載入場景與退出場景
+    /// </summary>
+    public class SceneControl : MonoBehaviour
+    {
+        /// <summary>
+        /// 載入場景
+        /// </summary>
+        /// <param name="sceneName">載入場景</param>
+        public void LoadScene(string sceneName)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+
+        /// <summary>
+        /// 退出場景
+        /// </summary>
+        public void QuitGame() //測試
+        {
+            Application.Quit();
+        }
+    }
+
+}
